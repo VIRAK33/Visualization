285366d8dc20b80568e3ae8c9f0d5b6f8e873c88
virak.itc <seamvirak.itc@gmail.com>
2020-12-15 23:06:35 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, url_for, request, render_template
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+auth = HTTPBasicAuth()
+
+users = {
+    "admin": generate_password_hash("123"),
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, %s!" % auth.current_user()
+
+if __name__ == '__main__':
+    app.run(debug = True)
